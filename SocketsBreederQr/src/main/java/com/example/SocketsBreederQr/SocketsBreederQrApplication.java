210edf90efd1b8972a4381046856d22cc4e04829
Rextro616 <111914475+Rextro616@users.noreply.github.com>
2024-01-17 08:48:04 -0600

    Add files via upload

---
+++ SocketsBreederQr/src/main/java/com/example/SocketsBreederQr/SocketsBreederQrApplication.java
@@ -0,0 +1,13 @@
+package com.example.SocketsBreederQr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketsBreederQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketsBreederQrApplication.class, args);
+	}
+
+}
---
+++ SocketsBreederQr/src/test/java/com/example/SocketsBreederQr/SocketsBreederQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocketsBreederQr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketsBreederQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
